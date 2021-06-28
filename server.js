6163281d05a59bf94e12f0706fa6cb073db8dca5
Maciej Wolak <74461485+WolakMaciej@users.noreply.github.com>
2021-06-28 21:59:42 +0200

    Update server.js
    change server.js distDir

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const path = require('path');
 
 const app = express();
 
+
+
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(distDir +"index.html"));
 });
 
 // Start the app by listening on the default Heroku port
